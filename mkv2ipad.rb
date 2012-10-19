30f28e7e7a11d921c873cc036e94c39c5f203c57
Erik Thorselius <erik.thorslius@gmail.com>
2012-10-19 21:46:44 +0200

    Added search for all mkv files in argv

--- mkv2ipad.rb
+++ mkv2ipad.rb
@@ -1,9 +1,23 @@
 #!/usr/bin/env ruby
-def find_mkv_files path
-ARGV.each do |path|
-  if File.directory? path
-    puts "Dir"
-  else
-    puts "file"
+require 'find'
+def find_mkv_files argv
+  files = []
+  Find.find(argv) do |path|
+    if FileTest.directory?(path)
+      if File.basename(path)[0] == "."
+        Find.prune       # Don't look any further into this directory.
+      else
+        next
+      end
+    else
+      if File.extname(path).downcase == ".mkv"
+        files << path
+      end
+    end
   end
+  files
+end
+
+ARGV.each do |path|
+  puts find_mkv_files path
 end
