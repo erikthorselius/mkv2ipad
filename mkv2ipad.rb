d307a8d15266291742666dc04cd71acca1bdca3d
Erik Thorselius <erik.thorslius@gmail.com>
2012-10-19 22:08:52 +0200

    added basic shell interaction to ffmpeg

--- mkv2ipad.rb
+++ mkv2ipad.rb
@@ -11,13 +11,18 @@ def find_mkv_files argv
       end
     else
       if File.extname(path).downcase == ".mkv"
-        files << path
+        fork_to_ffmpeg(path,ipad_file_name(path))
       end
     end
   end
   files
 end
 
+def fork_to_ffmpeg(input_file, output_file) 
+end
+def ipad_file_name(input_file)
+  puts input_file
+end
 ARGV.each do |path|
   puts find_mkv_files path
 end
