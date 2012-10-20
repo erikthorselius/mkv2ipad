6998cd428b398049d270fee53331a4c4c6ec02ae
Erik Thorselius <erik.thorslius@gmail.com>
2012-10-20 13:30:16 +0200

    Added so mp4 file gets removed if it is less then 15% of mkv size

--- mkv2ipad.rb
+++ mkv2ipad.rb
@@ -12,6 +12,7 @@ def find_mkv_files argv
       end
     else
       if File.extname(path).downcase == ".mkv"
+        rm_if_exist(path,ipad_file_name(path))
         fork_to_ffmpeg(path,ipad_file_name(path))
       end
     end
@@ -19,6 +20,14 @@ def find_mkv_files argv
   files
 end
 
+def rm_if_exist(input_file, output_file) 
+  unless(File.size?(input_file).nil? or File.size?(output_file).nil?)
+    if(File.size?(output_file) < File.size?(input_file)*0.15)
+      puts "Remove output_file because it's to small"
+      File.delete(output_file)
+    end
+  end
+end
 def fork_to_ffmpeg(input_file, output_file) 
   options = "-i #{input_file} -acodec aac -ac 2 -strict experimental -ab 160k -s 1024x768 -vcodec libx264 -preset slow -level 31 -maxrate 10000000 -bufsize 10000000 -b 1200k -f mp4 -threads 0 outputfile"
   Open3.popen3("ffmpeg", "-i", input_file, "-acodec", "aac", "-ac", "2", "-strict", "experimental","-ab","161k","-s","1024x768","-vcodec","libx264","-preset","slow","-level","31","-maxrate","10000000","-bufsize","10000000","-b","1200k","-f","mp4","-threads","0" , output_file) {|stdin, stdout, stderr, wait_thr|
@@ -33,7 +42,7 @@ def fork_to_ffmpeg(input_file, output_file)
   }
 end
 def ipad_file_name(input_file)
-  filename = File.basename(input_file.downcase,".mkv")+".ipad.mp4"
+  filename = File.basename(input_file,".mkv")+".ipad.mp4"
   File.join(File.dirname(input_file),filename)
 end
 ARGV.each do |path|
