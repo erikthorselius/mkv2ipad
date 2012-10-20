c86dbb28b5509c4e0771eafe49e543ea6f40fdab
Erik Thorselius <erik.thorslius@gmail.com>
2012-10-20 16:06:36 +0200

    added more info from ffmpeg

--- mkv2ipad.rb
+++ mkv2ipad.rb
@@ -32,12 +32,17 @@ def rm_if_to_small(input_file, output_file)
 end
 def fork_to_ffmpeg(input_file, output_file) 
   puts "#{Time.now.ctime}: processing #{input_file}"
-  Open3.popen3("ffmpeg", "-i", input_file, "-acodec", "aac", "-ac", "2", "-strict", "experimental","-ab","161k","-s","1024x768","-vcodec","libx264","-preset","slow","-level","31","-maxrate","10000000","-bufsize","10000000","-b","1200k","-f","mp4","-threads","0" , output_file) {|stdin, stdout, stderr, wait_thr|
+  Open3.popen3("ffmpeg", "-i", input_file, "-acodec", "aac", "-ac", "2", "-strict", "experimental","-ab","161k","-s","1024x768","-vcodec","libx264","-preset","slow","-level","31","-maxrate","10000000","-bufsize","10000000","-b","1200k","-f","mp4","-threads","0" , output_file) do |stdin, stdout, stderr, wait_thr|
     pid = wait_thr.pid # pid of the started process.
-    puts stdout.read.chomp
-    puts stderr.read.chomp
+    stdin.close_write
+    stdout.read.split("\n").each do |line|
+      puts "[ffmpeg] stdout: #{line}"
+    end
+    stderr.read.split("\n").each do |line|
+      puts "[ffmpeg] stderr: #{line}"
+    end
     exit_status = wait_thr.value # Process::Status object returned.
-  }
+  end
 end
 def ipad_file_name(input_file)
   filename = File.basename(input_file,".mkv")+".ipad.mp4"
