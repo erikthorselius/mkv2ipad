9d00a1590ba626e0fb4d12244b918723c4653d7e
Erik Thorselius <erik.thorslius@gmail.com>
2012-10-20 16:55:58 +0200

    error on close flush

--- mkv2ipad.rb
+++ mkv2ipad.rb
@@ -34,10 +34,12 @@ def fork_to_ffmpeg(input_file, output_file)
   puts "#{Time.now.ctime}: processing #{input_file}"
   Open3.popen3("ffmpeg", "-i", input_file, "-acodec", "aac", "-ac", "2", "-strict", "experimental","-ab","161k","-s","1024x768","-vcodec","libx264","-preset","slow","-level","31","-maxrate","10000000","-bufsize","10000000","-b","1200k","-f","mp4","-threads","0" , output_file) do |stdin, stdout, stderr, wait_thr|
     #pid = wait_thr.pid # pid of the started process.
-    stdin.close
     stdin.flush
     stdout.flush
     stderr.flush
+    stdin.close
+    stdout.close
+    stderr.close
     #exit_status = wait_thr.value # Process::Status object returned.
   end
 end
